0124085ae57f61fb0f695e76921a8c611f74d5b7
Aljomaa <sybhunter21@gmail.com>
2025-09-28 10:49:08 -0400

    Add files via upload

--- admin/manage_users.py
+++ admin/manage_users.py
@@ -1,1 +1,20 @@
+from config import load_users, save_users
 
+def add_user(user_id):
+    users = load_users()
+    if user_id not in users:
+        users.append(user_id)
+        save_users(users)
+        return True
+    return False
+
+def remove_user(user_id):
+    users = load_users()
+    if user_id in users:
+        users.remove(user_id)
+        save_users(users)
+        return True
+    return False
+
+def list_users():
+    return load_users()
